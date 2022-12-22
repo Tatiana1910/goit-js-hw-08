3d790cbb9395f8bfb9fd2bac2517b79d0a2b8d8b
Tatiana Kramarenko <tanya19101996@gmail.com>
2022-12-22 21:19:22 +0200

    add library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,58 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(SimpleLightbox);
+
+// const galleryEl = document.querySelector('.gallery');
+
+// const galleryMarkup = galleryItems
+//   .map(
+//     ({ preview, original, description }) =>
+//       `<div class="gallery__item">
+// <a class="gallery__link" href="${original}">
+//   <img
+//     class="gallery__image"
+//     src="${preview}"
+//     data-source="${original}"
+//     alt="${description}"
+//   />
+// </a>
+// </div>`
+//   )
+//   .join('');
+
+// galleryEl.innerHTML = galleryMarkup;
+
+// galleryEl.addEventListener('click', onGalleryClick);
+
+// function onGalleryClick(evt) {
+//   evt.preventDefault();
+
+//   if (!evt.target.classList.contains('gallery__image')) {
+//     return;
+//   }
+
+//   let instance = basicLightbox.create(
+//     `
+//   <img
+//     class="gallery__image"
+//     src="${evt.target.dataset.source}"
+//     alt="${evt.target.alt}"
+//   />
+// `
+//   );
+
+//   instance.show();
+
+//   galleryEl.addEventListener('keydown', evt => {
+//     if (evt.key === 'Escape') {
+//       instance.close();
+//       galleryEl.removeEventListener('keydown', onGalleryClick);
+//     }
+//   });
+// }
