a35ab944030d6d5a58d15d698b57d6d2410160d8
Tatiana Kramarenko <tanya19101996@gmail.com>
2022-12-22 21:40:40 +0200

    task-1_done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,51 +8,28 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // console.log(SimpleLightbox);
 
-// const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector('.gallery');
 
-// const galleryMarkup = galleryItems
-//   .map(
-//     ({ preview, original, description }) =>
-//       `<div class="gallery__item">
-// <a class="gallery__link" href="${original}">
-//   <img
-//     class="gallery__image"
-//     src="${preview}"
-//     data-source="${original}"
-//     alt="${description}"
-//   />
-// </a>
-// </div>`
-//   )
-//   .join('');
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div>
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    </div>`
+  )
+  .join('');
 
-// galleryEl.innerHTML = galleryMarkup;
+galleryEl.innerHTML = galleryMarkup;
 
-// galleryEl.addEventListener('click', onGalleryClick);
+galleryEl.addEventListener('click', onGalleryClick);
 
-// function onGalleryClick(evt) {
-//   evt.preventDefault();
+function onGalleryClick(evt) {
+  evt.preventDefault();
+}
 
-//   if (!evt.target.classList.contains('gallery__image')) {
-//     return;
-//   }
-
-//   let instance = basicLightbox.create(
-//     `
-//   <img
-//     class="gallery__image"
-//     src="${evt.target.dataset.source}"
-//     alt="${evt.target.alt}"
-//   />
-// `
-//   );
-
-//   instance.show();
-
-//   galleryEl.addEventListener('keydown', evt => {
-//     if (evt.key === 'Escape') {
-//       instance.close();
-//       galleryEl.removeEventListener('keydown', onGalleryClick);
-//     }
-//   });
-// }
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,2 @@
 import Player from '@vimeo/player';
-console.log(Player);
+// console.log(Player);
