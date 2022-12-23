e7be709c0c4a5508d3106d78d370ccfd66ba18e0
Tatiana Kramarenko <tanya19101996@gmail.com>
2022-12-23 11:36:39 +0200

    task-2_done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,21 @@
 import Player from '@vimeo/player';
-// console.log(Player);
+import throttle from 'lodash.throttle';
+console.log(Player);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
+}
+setCurrentTime();
+
+function setCurrentTime() {
+  if (!localStorage.getItem(CURRENT_TIME)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+}
