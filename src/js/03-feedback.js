c7ec045b7e600c8f8eb58b26083f714c68d71440
Tatiana Kramarenko <tanya19101996@gmail.com>
2022-12-24 20:53:35 +0200

    task-3_done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'selectedFilters';
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+let formData = {};
+
+onPageLoad();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+
+  console.dir(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onPageLoad() {
+  const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (savedData) {
+    formEl.email.value = savedData.email;
+    formEl.message.value = savedData.message;
+  }
+}
